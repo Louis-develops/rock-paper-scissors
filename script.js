5ab6079a40e4c3ee2cc13cf6c40cc48fd6fa08a4
Louis Sutherland <lsutherland263@gmail.com>
2020-11-12 12:04:10 +0000

    Fixed app when unable to recognize capitalized word inputs

--- script.js
+++ script.js
@@ -1,101 +1,128 @@
-
-var UIcontrol = (function(){
-    var DOMstrings = {
-        textInput: document.querySelector("#textInput"),
-        submitButton: document.querySelector("#submit")
-
+//1. GETTING DATA FROM THE UI AND THE COMPUTER
+let model = (function() {
+    
+    // dom strings:
+    const domStrings = {
+        userInput: "#textInput",
+        submit: "#submit",
+        gamePlay: ".gameplay"
     }
+        return {
 
+            // A. Get user input
+            getUserInput: function(){
+
+                return document.querySelector(domStrings.userInput).value;
+               
+            },
+
+            // B. Get computer input
+            compInput: function() {
+                let optionsArray = ["rock", "paper", "scissors"];
+                let randNum = Math.floor(Math.random()* 3);
+                console.log(randNum)
+                return optionsArray[randNum];
+            },
+
+            // C. Make DOM elements public
+            getDomStrings: function(){
+                return domStrings;
+            }
+        }
+})();
+
+// 2. USER INTERFACE CONTROLLER
+let UI = (function(){
+    
     return {
-        getInput: function(){
-            DOMstrings.submitButton.addEventListener("click", function(e){
-                e.preventDefault();
-                return DOMstrings.textInput.value;
-            })
+        insertHTML: function(outcome, playerScore, computerScore){
+            return `
+                <div class="text">
+                    <p>${outcome}! Score: Player ${playerScore} - ${computerScore} Computer</p>
+                </div>
+            `
+        },
+
+        gameOver: function(outcome, playerScore, computerScore){
+            return `
+                <div class="text">
+                    <p>GAME OVER - ${outcome}! FINAL SCORE: Player ${playerScore} - ${computerScore} Computer</p>
+                </div>
+            `
         }
     }
-
-    
-  
-
 })()
 
+// 3. APP CONTROLLER
+let controller = (function(mdl, ui){
+    // (imported domStrings from model):
+    let DOM = mdl.getDomStrings();
+    let gamePlay = document.querySelector(DOM.gamePlay);
+    let userInput = "scissors";
+    let compInput;
+    let score = {
+        player: 0,
+        computer: 0
+    }
 
+    console.log(gamePlay)
 
+    // a. Get user Input
 
-function checker(uictrl){
-    
-    var playerScore = 0;
-    var computerScore = 0;
-    for(var i = 0; i < 50; i++){
-        var inputComputer = "";
-        var randomNumber = Math.floor(Math.random() * 3);
-            if (randomNumber === 1){
-                inputComputer = "rock";
-            } else if (randomNumber === 2){
-                inputComputer = "paper";
-            } else {
-                inputComputer = "scissors";
+        document.querySelector(DOM.submit).addEventListener("click", (e) => {
+            e.preventDefault();
+            userInput = mdl.getUserInput();
+
+            // b. get computer Input
+                compInput = mdl.compInput();
+
+            // c. check whether the player wins, the computer wins or if its a draw
+            if(score.player !== 5 && score.computer !== 5){
+                if(userInput === compInput){
+                    gamePlay.insertAdjacentHTML("beforeend", ui.insertHTML("Draw", score.player, score.computer));
+                } else if(userInput === "rock" || userInput === "Rock" && compInput === "paper"){
+                    score.computer += 1;
+                    gamePlay.insertAdjacentHTML("beforeend", ui.insertHTML("Computer wins", score.player, score.computer));
+                } else if(userInput === "rock" || userInput === "Rock" && compInput === "scissors"){
+                    score.player += 1;
+                    gamePlay.insertAdjacentHTML("beforeend", ui.insertHTML("Player wins", score.player, score.computer));
+                } else if(userInput === "paper" || userInput === "Paper" && compInput === "scissors"){
+                    score.computer += 1;
+                    gamePlay.insertAdjacentHTML("beforeend", ui.insertHTML("Computer wins", score.player, score.computer));
+                } else if (userInput === "paper" || userInput === "Paper" && compInput === "rock"){
+                    score.player += 1;
+                    gamePlay.insertAdjacentHTML("beforeend", ui.insertHTML("Player wins", score.player, score.computer));
+                } else if (userInput === "scissors" || userInput === "Scissors" && compInput === "rock"){
+                    score.player += 1;
+                    gamePlay.insertAdjacentHTML("beforeend", ui.insertHTML("Player wins", score.player, score.computer));
+                } else if (userInput === "rock" || userInput === "Rock" && compInput === "paper"){
+                    score.computer += 1;
+                    gamePlay.insertAdjacentHTML("beforeend", ui.insertHTML("Computer wins", score.player, score.computer));
+                } else if (userInput === "scissors" || userInput === "Scissors" && compInput === "paper"){
+                    score.computer += 1;
+                    gamePlay.insertAdjacentHTML("beforeend", ui.insertHTML("Computer wins", score.player, score.computer));
+                }
+            } else if (score.player === 5 && score.computer < 5){
+                gamePlay.insertAdjacentHTML("beforeend", ui.gameOver("Player wins", score.player, score.computer));
+            } else if (score.player === 5 && score.computer === 5){
+                gamePlay.insertAdjacentHTML("beforeend", ui.gameOver("Draw", score.player, score.computer));
+            } else if (score.computer === 5 && score.player < 5){
+                gamePlay.insertAdjacentHTML("beforeend", ui.gameOver("Computer wins", score.player, score.computer));
             }
-    if(playerScore === 5 || computerScore === 5){
-        if(playerScore > computerScore){
-            console.log("GAME OVER - Player Wins " + playerScore + " - " + computerScore + "!!!" );
-        } else if (computerScore > playerScore){
-            console.log("GAME OVER - Computer wins " + computerScore + " - " + playerScore + "!!!" );
-        } else {
-            console.lot("GAME OVER - Draw " + playerScore + " - " + computerScore);
-        }
-        break;
-    } else {
-    var inputPlayer = uictrl.getInput();
-    if (inputPlayer == "rock" && inputComputer == "scissors"){
-        playerScore ++;
-        console.log("Player Wins!");
-        console.log("***************");
-        console.log("Total scores: Player " + playerScore + " - " + computerScore + " Computer");
-        console.log("***************");
-        
-    } else if (inputPlayer === "exit"){
-        break;
-    } else if (inputPlayer == "rock" && inputComputer == "paper"){
-                 computerScore ++;
-                 console.log("Computer Wins!");
-                 console.log("***************");
-                 console.log("Total scores: Player " + playerScore + " - " + computerScore + " Computer");
-                 console.log("***************");
-    } else if (inputPlayer == "paper" && inputComputer == "scissors"){
-               computerScore ++;
-                 console.log("Computer Wins!");
-                 console.log("***************");
-                 console.log("Total scores: Player " + playerScore + " - " + computerScore + " Computer");
-                 console.log("***************");
-    } else if (inputPlayer == "paper" && inputComputer == "rock"){
-               playerScore ++;
-               console.log("Player Wins!");
-                console.log("***************");
-                console.log("Total scores: Player " + playerScore + " - " + computerScore + " Computer");
-                console.log("***************");
-    } else if (inputPlayer == "scissors" && inputComputer == "rock"){
-                computerScore ++;
-                 console.log("Computer Wins!");
-                 console.log("***************");
-                 console.log("Total scores: Player " + playerScore + " - " + computerScore + " Computer");
-                 console.log("***************");
-    } else if (inputPlayer == "scissors" && inputComputer == "paper"){
-                playerScore ++;
-               console.log("Player Wins!");
-                console.log("***************");
-                console.log("Total scores: Player " + playerScore + " - " + computerScore + " Computer");
-                console.log("***************");
-    } else { console.log("Draw!");
-             console.log("***************");
-             console.log("Total scores: Player " + playerScore + " - " + computerScore + " Computer");
-             console.log("***************");
-           } 
-    }
+                
+        });
     
-    
-    }
-}
+})(model, UI)
 
-checker(UIcontrol);
+/**
+ * TODO LIST
+ * 1. Put end of game if else statements into a function to check at each if else statement (maybe?);
+ * 2. Add another line to the sentence that states the choice from the user and computer e.g: User input: Rock. Computer input: Scissors.
+ * 3. Remove the first HTML element if there are too many on the screen
+ * 5. Add the possibility to change the score limit
+ * 6. Fix the width of the GAME OVER string so that it goes onto a new line (play game until win to see error)
+ * 7. Add the possibility of a second player (way in future)
+ */
+
+
+
